8142d94e0f343593ba2336d8f0bb7f1ef14f628d
Podul <ylpodul@gmail.com>
2018-03-27 18:23:52 +0800

    找回丢失的代码

--- Package.swift
+++ Package.swift
@@ -1,21 +1,39 @@
 // swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// 软件包管理
 import PackageDescription
+
+let urls = [
+    "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",      // HTTP服务
+    "https://github.com/PerfectlySoft/Perfect-MySQL.git",           // MySQL服务
+    "https://github.com/PerfectlySoft/Perfect-Mustache.git"         // Mustache
+]
+
+
+//let package = Package(
+//    name: "Perfect",
+//    targets: [],
+//    dependencies: urls.map { .Package(url: $0, versions: Version(3,0,0)..<Version(4,0,0)) }
+//)
+
 
 let package = Package(
     name: "Perfect",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-         .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
-//         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-    ],
+        // HTTP服务
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+        // MySQL服务
+        .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
+        //         .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2)
+        // Mustache
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Perfect",
-            dependencies: ["PerfectHTTPServer"]),
-    ]
+            dependencies: ["PerfectHTTPServer", "PerfectMySQL", "PerfectMustache"]),
+        ]
 )
--- Sources/Perfect/main.swift
+++ Sources/Perfect/main.swift
@@ -1,12 +1,34 @@
-//import PerfectHTTP
-//import PerfectHTTPServer
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
 
-//HTTP服务
+// HTTP服务
 var routesArr = [[String: Any]]()
 
-var someDict1 : [String:String] = ["method":"GET","url":"/api"]
+var someDict1 = ["method": "GET", "url": "/api"]
 
 routesArr.append(someDict1)
+
 let networkServer = NetworkServerManager(root: "webroot", port: 8080, aRoutes: routesArr)
+
 networkServer.startServer()
+
+
+
+//// 注册您自己的路由和请求／响应句柄
+//var routes = Routes()
+//routes.add(method: .get, uri: "/") {
+//    request, response in
+//    response.setHeader(.contentType, value: "text/html")
+//    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+//        .completed()
+//}
+//
+//do {
+//    // 启动HTTP服务器
+//    try HTTPServer.launch(
+//        .server(name: "www.example.ca", port: 8181, routes: routes))
+//} catch {
+//    fatalError("\(error)") // fatal error launching one of the servers
+//}
 
